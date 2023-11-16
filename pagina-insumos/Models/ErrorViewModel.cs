1f472d703cb8fcdfb231ca83f298a1623ce50a0d
Leonardo Alves <leonardo.freitas.1429449@sga.ucminas.br>
2023-11-16 16:30:54 -0300

    Adicionar arquivos de projeto.

---
+++ pagina-insumos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pagina_insumos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
